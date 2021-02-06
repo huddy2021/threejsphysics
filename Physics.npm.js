f9b47db9e18c9ee5e11e924ada9a33c95786abb3
huddy2021 <78650784+huddy2021@users.noreply.github.com>
2021-02-06 14:48:38 +0000

    Update Physics.npm.js

--- Physics.npm.js
+++ Physics.npm.js
@@ -5,6 +5,7 @@ var Physics = function(config)
      this.scene = cfg.scene;
      this.gravity = cfg.gravity || 9.82; //m/s strength.
      this.groundItem = cfg.ground || new THREE.PlaneGeometry(1 , 1 , 1);
+     this.fps = cfg.fpsLimit || 60; //uses this to work out speeds.
      
      this.enabled = true;
      
@@ -19,7 +20,27 @@ var Physics = function(config)
         
      }
      this.updateMatrix = function(){
-
+          this.scene.children.forEach((child) => {
+              
+              if(!child.velocity)
+              {
+                   return;    
+              }
+              if(!child.physics)
+              {
+                   return;    
+              }
+               
+              //velocity is m/s.
+              var velocityY = (child.velocity.y / this.fps);
+              child.translateY((velocityY - (this.gravity / this.fps)));
+               
+              var velocityX = (child.velocity.x / this.fps);
+              child.translateX((velocityX / this.fps));
+               
+              var velocityZ = (child.velocity.z / this.fps);
+              child.translateZ(velocityZ);
+          });
      }
 };
 
